37cd2be39bbce3316869eab55d1e30aa6e848287
DogeOfFreedom <jchensi2000@gmail.com>
2024-02-21 13:46:00 +1100

    feat: add responsive formatting of weather data

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,19 +1,11 @@
 import "../style.css";
 
 const weatherForm = document.querySelector(".weather-form");
-const forecastContent = document.querySelector(".forecast-container");
-const searchInput = document.querySelector("input");
+const weatherContent = document.querySelector(".weather-data-container");
+// const searchInput = document.querySelector("input");
 const loader = document.querySelector(".loader");
 const errMsg = document.querySelector(".err-msg");
-const days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
+const days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 const months = [
   "Jan",
   "Feb",
@@ -40,33 +32,45 @@ const populateWeatherData = (forecastData) => {
     const minTempCelcius = forcast.day.mintemp_c;
     const humidity = forcast.day.avghumidity;
 
-    const forecastDay = document.createElement("div");
-    forecastDay.className = "forecast-day";
+    const weatherItem = document.createElement("div");
+    weatherItem.className = "weather-item";
+
     const dayElement = document.createElement("h1");
+    dayElement.className = "day";
     dayElement.textContent = `${dayName} ${months[date[0] - 1]} ${date[1]}`;
+
+    const conditionContainer = document.createElement("div");
+    conditionContainer.className = "condition-container";
     const conditionIcon = document.createElement("img");
     conditionIcon.src = conditionIconSrc;
     const conditionElement = document.createElement("p");
     conditionElement.textContent = condition;
+    conditionContainer.appendChild(conditionIcon);
+    conditionContainer.appendChild(conditionElement);
+
+    const metricContainer = document.createElement("div");
+    metricContainer.className = "metric-container";
     const maxTempElement = document.createElement("p");
     maxTempElement.innerHTML = `Max Temperature: ${maxTempCelcius} &#8451`;
     const minTempElement = document.createElement("p");
     minTempElement.innerHTML = `Min Temperature: ${minTempCelcius} &#8451`;
     const humidityElement = document.createElement("p");
     humidityElement.textContent = `Humidity: ${humidity}%`;
+    metricContainer.appendChild(maxTempElement);
+    metricContainer.appendChild(minTempElement);
+    metricContainer.appendChild(humidityElement);
 
-    forecastDay.appendChild(dayElement);
-    forecastDay.appendChild(conditionIcon);
-    forecastDay.appendChild(conditionElement);
-    forecastDay.appendChild(maxTempElement);
-    forecastDay.appendChild(minTempElement);
-    forecastDay.appendChild(humidityElement);
-    forecastContent.appendChild(forecastDay);
+    weatherItem.appendChild(dayElement);
+    weatherItem.appendChild(conditionContainer);
+    weatherItem.appendChild(metricContainer);
+    weatherContent.appendChild(weatherItem);
   });
 };
 
 const getWeatherData = async () => {
-  const searchTerm = searchInput.value;
+  // const searchTerm = searchInput.value;
+  const searchTerm = "singapore";
+
   const key = "30bd2286b09543ebbbe233108241702";
   const url = `http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${searchTerm}&days=7&aqi=no&alerts=no`;
   const response = await fetch(url, { mode: "cors" });
@@ -96,7 +100,7 @@ weatherForm.addEventListener("submit", (e) => {
     errMsg.textContent = "";
 
     // clean up previous data
-    forecastContent.innerHTML = "";
+    weatherContent.innerHTML = "";
 
     // show loader
     loader.classList.remove("hidden");
@@ -104,3 +108,5 @@ weatherForm.addEventListener("submit", (e) => {
     getWeatherData();
   }
 });
+
+getWeatherData();
